117b7e63a86ccc9879d295419895198962e40e65
dojekon <32470113+dojekon@users.noreply.github.com>
2024-03-26 17:20:06 +0300

    Изменения во фронте

--- soup_system_settings/static/place/js/place_screen.js
+++ soup_system_settings/static/place/js/place_screen.js
@@ -6,9 +6,12 @@ let main_number = document.getElementById('main_number');
 
 let not_active_title = document.getElementById('not-active');
 
+let pause_title = document.getElementById('pause');
+
 let doctor_name  = document.getElementById('doctor_name');
 
 let place_name_title = document.getElementById('place-name')
+
 
 let checker = 0
 
@@ -40,6 +43,15 @@ placeSocket.onmessage = function(event) {
         return
     }
 
+    // Врач зарегался на месте
+    if (data.fio && data.fio !== "Идут сопутствующие процедуры") {
+        let place_doctor_fio = data.fio
+        doctor_name.textContent = `Принимающий врач: ${place_doctor_fio}`
+        not_active_title.classList.add('none-active') 
+        pause_title.classList.add('none-active') 
+    }
+
+    // Врач ушёл - кабинет закрывается
     if (data.close) { 
         main_number.textContent = '' 
         not_active_title.classList.remove('none-active') 
@@ -47,29 +59,27 @@ placeSocket.onmessage = function(event) {
         return
     }
 
+    // Врач взял перерыв
     if (data.break) { 
         console.log('break')
-        main_number.textContent = 'Проветривание'
+        pause_title.classList.remove('none-active') 
+        main_number.classList.add('none-active') 
+        not_active_title.classList.add('none-active')
         return  
     }
-
-    console.log(data.fio)
 
     if (data.fio == "Идут сопутствующие процедуры") { 
         doctor_name.textContent = ''
         not_active_title.classList.add('none-active') 
     }
 
-
-    if (data.fio && data.fio !== "Идут сопутствующие процедуры") {
-        let place_doctor_fio = data.fio
-        doctor_name.textContent = `Принимающий врач: ${place_doctor_fio}`
-        not_active_title.classList.add('none-active') 
-    }
+    console.log(data.fio)
 
     let place_main_number = data.next_number
     console.log(place_main_number)
     main_number.textContent = place_main_number 
+    main_number.classList.remove('none-active') 
+    pause_title.classList.add('none-active') 
     if (place_main_number === 0) { 
         console.log("FDFSF")
         main_number.textContent = ''
